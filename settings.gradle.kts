e726a8042173b9dd9c182f7bef2fb91ca0832270
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2025-04-21 16:26:28 +0200

    Created project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.gradle.develocity") version "3.19.2"
+}
+
+dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+
+rootProject.name = "ise-lab-code-jason"
+
+include("helloworld")
+include("computation")
+include("interaction")
+include("fanboy")
+include("domotic")
+include("thermostat")
+include("robots")
+include("contractnet")
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+        publishing {
+            onlyIf {
+                it.buildResult.failures.isNotEmpty()
+            }
+        }
+    }
+}
---
+++ thermostat/build/resources/main/thermostat_agent.asl
@@ -0,0 +1,24 @@
+target(20).
+
++temperature(X) <- !regulate_temperature(X).
+
++random <- .print("Random percept detected").
+-random <- .print("Random percept removed").
+
+
++!regulate_temperature(X) : target(Y) & X - Y > 0.5 <-
+    .print("Temperature is ", X, ": need to cool down");
+    spray_air(cold).
+
++!regulate_temperature(X) : target(Y) & Y - X > 0.5 <-
+    .print("Temperature is ", X, ": need to warm up");
+    spray_air(hot).
+
++!regulate_temperature(X) : target(Y) & Z = X - Y & Z >= -0.5 & Z <= 0.5 <-
+    .print("Temperature is ", X, ": it's ok.").
+
+-!regulate_temperature(X) <-
+    .print("Failed to spray air. Retrying.");
+    !regulate_temperature(X).
+
+
